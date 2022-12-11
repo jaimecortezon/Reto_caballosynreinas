aaa04e4e0b3c6ef09d48b4d2eea1b8d7ddd09221
jaimecortezon <jaime.cortezon1@usp.ceu.es>
2022-12-11 12:37:38 +0100

    commit3

--- N-reinas.py
+++ N-reinas.py
@@ -1,31 +1,31 @@
 from __future__ import annotations
 
 
-def depth_first_search(
-    possible_board: list[int],
-    diagonal_right_collisions: list[int],
-    diagonal_left_collisions: list[int],
-    boards: list[list[str]],
+def profundidad_primera_busqueda(
+    posible_casilla: list[int],
+    colisiones_diagonal_dcha: list[int],
+    colisiones_diagonal_izq: list[int],
+    casillas: list[list[str]],
     n: int,
 ) -> None:
     """
-    >>> boards = []
-    >>> depth_first_search([], [], [], boards, 4)
-    >>> for board in boards:
-    ...     print(board)
+    >>> casilas = []
+    >>> profumdidas_primera_busqueda([], [], [], casillas, 4)
+    >>> for casilla in casillas:
+    ...     print(casilla)
     ['. Q . . ', '. . . Q ', 'Q . . . ', '. . Q . ']
     ['. . Q . ', 'Q . . . ', '. . . Q ', '. Q . . ']
     """
 
     # Get next row in the current board (possible_board) to fill it with a queen
-    row = len(possible_board)
+    row = len(posible_casilla)
 
     # If row is equal to the size of the board it means there are a queen in each row in
     # the current board (possible_board)
     if row == n:
         # We convert the variable possible_board that looks like this: [1, 3, 0, 2] to
         # this: ['. Q . . ', '. . . Q ', 'Q . . . ', '. . Q . ']
-        boards.append([". " * i + "Q " + ". " * (n - 1 - i) for i in possible_board])
+        casillas.append([". " * i + "Q " + ". " * (n - 1 - i) for i in posible_casilla])
         return
 
     # We iterate each column in the row to find all possible results in each row
@@ -45,37 +45,37 @@ def depth_first_search(
         # If any or these are True it means there is a collision so we continue to the
         # next value in the for loop.
         if (
-            col in possible_board
-            or row - col in diagonal_right_collisions
-            or row + col in diagonal_left_collisions
+            col in posible_casilla
+            or row - col in colisiones_diagonal_dcha
+            or row + col in colisiones_diagonal_izq
         ):
             continue
 
         # If it is False we call dfs function again and we update the inputs
-        depth_first_search(
-            possible_board + [col],
-            diagonal_right_collisions + [row - col],
-            diagonal_left_collisions + [row + col],
-            boards,
+        profundidad_primera_busqueda(
+            posible_casilla + [col],
+            colisiones_diagonal_dcha + [row - col],
+            colisiones_diagonal_izq + [row + col],
+            casillas,
             n,
         )
 
 
-def n_queens_solution(n: int) -> None:
-    boards: list[list[str]] = []
-    depth_first_search([], [], [], boards, n)
+def n_reinas_solucion(n: int) -> None:
+    casillas: list[list[str]] = []
+    profundidad_primera_busqueda([], [], [], casillas, n)
 
     # Print all the boards
-    for board in boards:
-        for column in board:
+    for casilla in casillas:
+        for column in casilla:
             print(column)
         print("")
 
-    print(len(boards), "solutions were found.")
+    print(len(casillas), "solutions were found.")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
-    n_queens_solution(4)
+    n_reinas_solucion(4)
